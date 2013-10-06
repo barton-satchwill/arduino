5a62f77436717a3d7dfde09e5b46e53148f1f2f6
Barton Satchwill <barton.satchwill@gmail.com>
2013-10-05 18:45:25 -0600

    working, but with worlds worst protocol

--- FSOCtest/FSOCtest.ino
+++ FSOCtest/FSOCtest.ino
@@ -31,36 +31,37 @@
  * 1       0       1       1:1024 prescaler
  ************************************************************************************************************/
 
-#define LED1 13
+#define LEDlaser 13
 #define LEDtx 12
 #define LEDrx 11
-#define LEDr 3
-#define LEDw 4
-#define rx 5
-#define tx 6
+#define LEDr 4
+#define LEDw 5
+#define rx 3
+#define tx 2
 #define no_such_pin 255
 #define PHOTOCELL A0
 #define LEDSENSOR A1
-#define SAMPLESIZE 5
+#define SAMPLESIZE 10
 //-------------------------
 volatile int EIGHT_HZ = 0;
-enum timer_action { WRITE, SAMPLE};
-char* timer_actions[] = {"WRITE", "SAMPLE"};
-volatile timer_action timerAction = WRITE;
+enum timer_action {TRANSMIT, RECIEVE};
+char* timer_actions[] = {"TX", "RX"};
+volatile timer_action timerAction = TRANSMIT;
 volatile timer_action tempAction;
 //-------------------------
-byte transmit = 13; //define our transmit pin
-byte data = 170; //value to transmit, binary 10101010
-byte mask = 1; //our bitmask
-int bitDelay = 100;
-
+int bitDelay = 5;
+int thisByte = '\~'; 
 long t;
+byte low = 0;
 
 SoftwareSerial mySerial(rx, tx); 
 
 void setup(){  
   setupTimer();
-  for (int i =3; i<=13;i++){ pinMode(i,OUTPUT); digitalWrite(i,LOW);}
+  for (int i =3; i<=13;i++){ 
+    pinMode(i,OUTPUT); 
+    digitalWrite(i,LOW);
+  }
   pinMode(A0, INPUT);
   pinMode(LEDSENSOR, INPUT);
   digitalWrite(LEDr, HIGH);
@@ -68,52 +69,41 @@ void setup(){
   Serial.begin(9600);
   mySerial.begin(9600);
   t = millis();
+  Serial.println("away we go...");
 }
 
-// extract bits from message
-// transmit bits by flashing an LED at some rate
-// sample the sensor at 10 x rate
-// assemble bits back into some message
-// compare data sent to data recieved
 
-while bytes
-  read a single byte
-    for each bit
-      by reading the timer
-        flash a bit
-        read the bit 10 times
-        write the bit
-
-void loop()
-{ 
-  if (timerAction == SAMPLE) {
-//    if (tempAction != timerAction) { tempAction = timerAction; Serial.println(timer_actions[timerAction]);}
-    int val = getSensorReading(LEDSENSOR);
-//    Serial.println(val);
-    digitalWrite(LEDrx,val);
-//    delay(200);
+void loop(){ 
+  //if (tempAction != timerAction) {tempAction = timerAction; Serial.println(timer_actions[timerAction]);}
+  //----------------------------------------------------------------------
+  if (timerAction == TRANSMIT) { 
+    if (Serial.available() == 0) {
+      mySerial.print(thisByte);   
+      Serial.print(thisByte);   
+      if(thisByte == 126) { thisByte = 32; } 
+      thisByte++;
+      Serial.print("-->");  
+    }
   }
-
-  
-  if (timerAction == WRITE) {
-    if (millis() - t > 200){t = millis(); digitalWrite(LEDtx, digitalRead(LEDtx)^1); digitalWrite(13, digitalRead(13)^1);}
-//    if (tempAction != timerAction) {tempAction = timerAction;  Serial.println(timer_actions[timerAction]);}
-//    if (Serial.available()){
-//      byte val = Serial.read();
-//      //printBits(val);
-//      mask = 00000001;
-//    }
-//    if (mask > 0){
-//      if (data & mask){ // if bitwise AND resolves to true
-//        digitalWrite(transmit,HIGH); 
-//        Serial.print("1");
-//      } else {     //if bitwise AND resolves to false
-//        digitalWrite(transmit,LOW); 
-//        Serial.print("0");
-//      }
-//      mask <<= 1;
-//      delay(bitDelay);
-//    }
+  //----------------------------------------------------------------------
+  if (timerAction == RECIEVE) {
+    if (Serial.available()){
+      byte data = Serial.read();
+      low = 0;
+      for (byte mask = 00000001; mask>0; mask <<= 1) {
+        digitalWrite(LEDlaser,(data & mask > 0));
+        digitalWrite(LEDtx,(data & mask > 0));
+        delay(bitDelay);
+        
+        // receive a single bit of transmitted data
+        int val = getSensorReading(LEDSENSOR);
+        low += (data & mask);
+        digitalWrite(LEDrx,val);
+        if(digitalRead(LEDrx) != digitalRead(LEDtx)) { Serial.print("x");}
+      }
+      Serial.print((char)low);
+      if (Serial.available()==0){Serial.println();}
+    }
   }
 }
 
@@ -125,12 +115,10 @@ int getSensorReading(int sensorPin){
     delay(1);
   }
   val = (val/SAMPLESIZE);
-//  Serial.print(val);
-//  Serial.print(" --> ");
-  if (val < 300) {
+  //Serial.println(val);
+  if (val < 420) {
     val = 0;
-  } 
-  else {
+  } else {
     val = 1;
   }
   return val;
@@ -138,19 +126,20 @@ int getSensorReading(int sensorPin){
 
 
 void printBits(byte data){
-  Serial.println();
-  Serial.print(data,BIN); Serial.print(" : "); Serial.println(data);
-  // iterate through bit mask
-  for (mask = 00000001; mask>0; mask <<= 1) { 
-    if (data & mask){ // if bitwise AND resolves to true
-      digitalWrite(transmit,HIGH); 
-//      Serial.print("1");
-    } else {     //if bitwise AND resolves to false
-      digitalWrite(transmit,LOW); 
-//      Serial.print("0");
+//  Serial.println();
+//  Serial.print(data,BIN); 
+//  Serial.print(" : "); 
+//  Serial.println(data);
+  for (byte mask = 00000001; mask>0; mask <<= 1) { 
+    if (data & mask){
+      digitalWrite(LEDlaser,HIGH); 
+      //Serial.print("1");
+    } else {
+      digitalWrite(LEDlaser,LOW); 
+      //Serial.print("0");
     }
-    //delayMicroseconds(bitDelay);
-    delay(bitDelay);
+    delayMicroseconds(bitDelay);
+//    delay(bitDelay);
   }
 }
 
@@ -163,7 +152,7 @@ void setupTimer() {
   TCCR2A = 0;              // set entire TCCR2A register to 0
   TCCR2B = 0;              // same for TCCR2B
   TCNT2  = 0;              //initialize counter value to 0
-  // set compare match register for 8khz increments
+                           // set compare match register for 8khz increments
   OCR2A = 249;             // = (16*10^6) / (1000 * 64) - 1 (must be <256)
   TCCR2A |= (1 << WGM21);  // turn on CTC mode
   TCCR2B |= (1 << CS12);   // | (1<<CS10);    // Set CS10 and CS12 bit for 64 prescaler
@@ -175,18 +164,19 @@ void setupTimer() {
 ISR(TIMER2_COMPA_vect){
   if (EIGHT_HZ++ == (100)) { 
     EIGHT_HZ = 0;
-        digitalWrite(LEDr, digitalRead(LEDr) ^ 1);
-        digitalWrite(LEDw, digitalRead(LEDw) ^ 1);
-    if (timerAction == SAMPLE){
-      timerAction = WRITE;
+    digitalWrite(LEDr, digitalRead(LEDr) ^ 1);
+    digitalWrite(LEDw, digitalRead(LEDw) ^ 1);
+    if (timerAction == RECIEVE){
+      timerAction = TRANSMIT;
     } 
     else {
-      timerAction = SAMPLE;
+      timerAction = RECIEVE;
     } 
   }
 }
 
 //--------------------------------------------------------------------------
+
 
 
 
