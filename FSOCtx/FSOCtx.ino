02e71981fe3fb8a4b36bc629ae74a7f7e605ae52
Barton Satchwill <barton.satchwill@gmail.com>
2013-10-06 21:30:06 -0600

    add preamble

--- FSOCtx/FSOCtx.ino
+++ FSOCtx/FSOCtx.ino
@@ -15,28 +15,52 @@ void setup() {
   Serial.begin(9600); 
   pinMode(LEDlaser, OUTPUT);
   pinMode(LEDtx, OUTPUT);
+  digitalWrite(LEDlaser, LOW);
+  digitalWrite(LEDtx, LOW);
   Serial.println("=========== Transmitter ===========");
 } 
 
 void loop() { 
-  for (byte data = '\!'; data <= '\~'; data++){
-    Serial.print("-->");  
-    Serial.print(" dec: ");  Serial.print(data, DEC);  
-    //Serial.print(", hex: ");  Serial.print(data, HEX); 
-    //Serial.print(", oct: ");  Serial.print(data, OCT);
-    Serial.print(", bin: ");  Serial.print(data, BIN);   
-    Serial.print("-->");  
-
-    for (byte mask = 00000001; mask>0; mask <<= 1) {
-      int val = ((data & mask) > 0);
-      digitalWrite(LEDlaser, val);
-      digitalWrite(LEDtx, val);
-      Serial.print(val);
-      delay(bitDelay);
-    }
-    Serial.println();
-  } 
+  preamble();
+//  for (byte data = '\!'; data <= '\~'; data++){
+//    transmit(data);
+//  }
 }
+
+void transmit(byte data) { 
+//----------- debugging -----------
+  Serial.print("[");  
+  Serial.write(data);
+  Serial.print("]-->");  
+  Serial.print(data, BIN);   
+  Serial.print("-->");  
+//---------------------------------
+  for (byte mask = 00000001; mask>0; mask <<= 1) {
+    int val = ((data & mask) > 0);
+    digitalWrite(LEDlaser, val);
+    digitalWrite(LEDtx, val);
+    Serial.print(val);
+    delay(bitDelay);
+  }
+  Serial.println();
+}
+
+
+void preamble(){
+  Serial.println("sending preamble");
+  for (int i=0; i<10; i++){
+    int val = (digitalRead(LEDlaser) ^1);
+    digitalWrite(LEDlaser, val);
+    digitalWrite(LEDtx, val);
+    Serial.print(val);
+    delay(500);
+  }
+  Serial.println();
+  transmit('\X');
+  digitalWrite(LEDlaser, LOW);
+  digitalWrite(LEDtx, LOW);
+}
+  
 
 
 
